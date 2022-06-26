064ec3c0a7832c61a404f5baa887c1b50cafd541
jagansai <jagan.sai@outlook.com>
2022-06-26 08:50:14 +0800

    Added CRUD project.

---
+++ UserRepo/src/main/java/com/edu/userms/UserMicroserviceApp.java
@@ -0,0 +1,13 @@
+package com.edu.userms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMicroserviceApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMicroserviceApp.class, args);
+	}
+
+}
---
+++ UserRepo/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into user_data (name) values ('Sai');
+insert into user_data (name) values ('Jagan');
+insert into user_data (name) values ('Neeraja');
+insert into user_data (name) values ('Kartikeya');
